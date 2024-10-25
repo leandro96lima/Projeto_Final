39c5b3e8c32b1c8912bb279809122a985aa04319
Hernán <hernani.arriscado@gmail.com>
2024-10-25 12:27:02 +0100

    ChangeTypeRequestApproval DONE

---
+++ app/Notifications/TypeChangeRequestNotification.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\TypeChangeRequest;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class TypeChangeRequestNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected $request; // Melhor prática: Usar protected para variáveis internas
+
+    /**
+     * Cria uma nova instância de notificação.
+     *
+     * @param TypeChangeRequest $request A solicitação de mudança de tipo.
+     */
+    public function __construct(TypeChangeRequest $request)
+    {
+        // Inicializa a propriedade $request com o objeto TypeChangeRequest fornecido
+        $this->request = $request;
+    }
+
+    /**
+     * Define os canais pelos quais a notificação será enviada.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Cria a mensagem de e-mail para a notificação.
+     *
+     * @param mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('New User Type Change Request')
+            ->line('A user has requested to change their type.')
+            ->line('Requested Type: ' . $this->request->requested_type)
+            ->line('Reason: ' . $this->request->reason)
+            ->action('Review Request', url('/admin/type-change-requests'))
+            ->line('Please review and approve or reject the request.');
+    }
+}
+
