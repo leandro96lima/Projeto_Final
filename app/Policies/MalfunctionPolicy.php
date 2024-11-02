b4749887e5ea4936491947317879a07f1d05ebc1
Arriscado <Arriscado@DESKTOP-VK4TQ8A>
2024-11-02 21:31:58 +0000

    Refactor MalfunctionController DONE

---
+++ app/Policies/MalfunctionPolicy.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class MalfunctionPolicy extends RolePolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function viewAny(User $user)
+    {
+        return parent::canViewAny($user);
+    }
+
+    /**
+     * Determine if the user can view a specific ticket.
+     */
+
+    public function delete(User $user)
+    {
+        return  $this->isAdmin($user);
+    }
+
+    public function update(User $user)
+    {
+        return  $this->isAdmin($user);
+    }
+}
