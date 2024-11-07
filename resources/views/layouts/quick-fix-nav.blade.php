7319fd3a6fd7e643464e2c946b6e5daf2d22a121
Hernán <hernani.arriscado@gmail.com>
2024-11-07 10:38:54 +0000

    featureFix quick-fix-nav.blade.php DONE

--- resources/views/layouts/quick-fix-nav.blade.php
+++ resources/views/layouts/quick-fix-nav.blade.php
@@ -4,23 +4,25 @@
 @include('layouts.avatar')
 
         <ul id="side_items">
+
             <li class="side-item"><a href="{{ route('profile.edit') }}"><i class="fa-solid fa-user"></i>
                     <span class="item-description">{{ __('Perfil') }}</span></a></li>
-            <li class="side-item"><a href="{{ route('tickets.create') }}"><i class="fa-solid fa-receipt"></i>
-                    <span class="item-description">{{ __('Registar Tickets') }}</span></a></li>
+
             <li class="side-item"><a href="{{ route('tickets.index') }}"><i class="fa-solid fa-list"></i>
                     <span class="item-description">{{ __('Tickets') }}</span></a></li>
+            @if(in_array(Auth::user()->getType(), ['User']))
+                <li class="side-item"><a href="{{ route('tickets.create') }}"><i class="fa-solid fa-receipt"></i>
+                        <span class="item-description">{{ __('Registar Tickets') }}</span></a></li>
+            @endif
             @if(in_array(Auth::user()->getType(), ['Admin']))
                 <li class="side-item"><a href="{{ route('admin.requests') }}"> <i class="fa-solid fa-bell"></i>
                         <span class="item-description">{{ __('Solicitações') }}</span></a></li>
-            @endif
-            @if(in_array(Auth::user()->getType(), ['Admin', 'Technician']))
+                <li class="side-item"><a href="{{ route('equipments.index') }}"><i class="fa-solid fa-desktop"></i>
+                        <span class="item-description"> {{ __('Equipamentos') }}</span></a></li>
                 <li class="side-item"><a href="{{ route('malfunctions.index') }}"><i class="fa-solid fa-file"></i>
                         <span class="item-description">{{ __('Relatórios') }}</span></a></li>
                 <li class="side-item"><a href="{{ route('technicians.index') }}"><i class="fa-solid fa-users"></i>
                         <span class="item-description">{{ __('Técnicos') }}</span></a></li>
-                <li class="side-item"><a href="{{ route('equipments.index') }}"><i class="fa-solid fa-desktop"></i>
-                        <span class="item-description"> {{ __('Equipamentos') }}</span></a></li>
             @endif
         </ul>
 
